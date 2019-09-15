8728405564517334cfa4950f7a56ca7f24b5cae4
lithium <code@hlidskialf.com>
2019-09-14 19:44:08 -0700

    refactor into singleton class

--- planner.user.js
+++ planner.user.js
@@ -22,58 +22,12 @@
 function wrapper(plugin_info) {
 // ensure plugin framework is there, even if iitc is not yet loaded
 if(typeof window.plugin !== 'function') window.plugin = function() {};
-
-
-
-
 // PLUGIN START ////////////////////////////////////////////////////////
 
 
-
-
-window.plugin.planner = function() {};
-
-window.plugin.planner.PlanItem = function(src, dest) {
-  this.src = src
-  this.dest = dest
-}
-window.plugin.planner.PlanItem.prototype.uniqid = function() {
-  return [llstring(this.src), llstring(this.dest)].sort().join("<=>");
-}
-
-window.plugin.planner.addLink = function(src, dest)  {
-  var newItem = new plugin.planner.PlanItem(src, dest);
-  console.log("PLAN addLink", newItem.uniqid())
-  if (this.items.findIndex(function(i) { return i.uniqid() == newItem.uniqid(); }) == -1) {
-    this.items.push(newItem)
-  }
-}
-window.plugin.planner.addDrawToolsLayer = function(layer) {
-  var latlngs = layer.latLngs || layer._latlngs
-  console.log("PLAN addDrawToolsLayer", layer, latlngs)
-
-  if (latlngs.length == 2) {
-    this.addLink(latlngs[0], latlngs[1]);
-  } else if (latlngs.length == 3) {
-    this.addLink(latlngs[0], latlngs[1]);
-    this.addLink(latlngs[0], latlngs[2]);
-    this.addLink(latlngs[1], latlngs[2]);
-  }
-}
-
-window.plugin.planner.loadPlanFromDrawtools = function(drawToolsItems) {
-
-  this.items = [];
-  drawToolsItems.forEach(function(drawItem) {
-    this.addDrawToolsLayer(drawItem);
-  }.bind(this));
-
-  console.log("PLAN items", this.items);
-
-}
-
-
-
+/*
+ * global utility functions
+ */
 
 var llstring = function(latlng) {
   if (typeof latlng.lat != 'undefined' && typeof latlng.lng != 'undefined')
@@ -82,41 +36,54 @@ var llstring = function(latlng) {
   }
 }
 
-window.plugin.planner.handlePortalAdded = function(data) {
-  var portal = data.portal;
-  var guid = portal.options.guid;
-  var ll = llstring( portal._latlng );
-  var title = portal.options.data.title || guid;
 
-  //console.log("PLAN portalAdded", data.portal, title)
+/*
+ * class PlanItem
+ */
 
-  this.portal_by_guid[guid] = portal
-  this.portal_by_ll[ll] = portal
-
-  if (this.items.map(function(i) { return [llstring(i.src), llstring(i.dest)]}).flat().includes(ll)) {
-    this.rerender();
-  }
+function PlanItem(src, dest) {
+  this.src = src
+  this.dest = dest
+}
+PlanItem.prototype.uniqid = function() {
+  return [llstring(this.src), llstring(this.dest)].sort().join("<=>");
 }
 
 
 
+/*
+ * class PlannerPlugin 
+ *    global plugin singleton
+ */
 
-
-window.plugin.planner.setup = function() {
-
+function PlannerPlugin() {
   this.portal_by_guid = {};
   this.portal_by_ll = {};
 
+  // iitc hooks
   window.addHook('portalAdded', this.handlePortalAdded.bind(this));
-
-
   window.pluginCreateHook('pluginDrawTools'); // initialize hook if needed first
   window.addHook('pluginDrawTools', this.handleDrawTools.bind(this));
 
+
+  // load initial plan
   this.loadPlanFromDrawtools(JSON.parse(localStorage['plugin-draw-tools-layer']))
 
-  $('#toolbox').append('<a tabindex="0" onclick="plugin.planner.showPlannerDialog();">Planner</a>');
 
+  this.setupDesktop();
+  this.setupMobile();
+}
+
+// plugin boot - called by iitc
+PlannerPlugin.boot = function() {
+  window.plugin.planner = new PlannerPlugin()
+}
+
+PlannerPlugin.prototype.setupDesktop = function() {
+  $('#toolbox').append('<a tabindex="0" onclick="plugin.planner.showPlannerDialog();">Planner</a>');
+}
+
+PlannerPlugin.prototype.setupMobile = function() {
   if (window.useAndroidPanes()) {
     this.mobilePane = document.createElement('div');
     this.mobilePane.className = 'plugin-planner-pane';
@@ -147,34 +114,104 @@ window.plugin.planner.setup = function() {
       });
     
     android.addPane('plugin-planner', 'Planner', 'ic_planner');
-
-
     addHook('paneChanged', this.handlePaneChanged.bind(this));
-
   }
+}
 
-
+PlannerPlugin.prototype.handlePaneChanged = function(pane) {
+  if(pane == 'plugin-planner') {
+    document.body.appendChild(this.mobilePane);
+  } else if(this.mobilePane.parentNode) {
+    this.mobilePane.parentNode.removeChild(this.mobilePane);
+  }
 };
 
-window.plugin.planner.portalFromLatlng = function(latlng) {
+PlannerPlugin.prototype.handlePortalAdded = function(data) {
+  var portal = data.portal;
+  var guid = portal.options.guid;
+  var ll = llstring( portal._latlng );
+  var title = portal.options.data.title || guid;
+
+  // console.log("PLAN portalAdded", data.portal, title)
+
+  this.portal_by_guid[guid] = portal
+  this.portal_by_ll[ll] = portal
+
+  if (this.items.map(function(i) { return [llstring(i.src), llstring(i.dest)]}).flat().includes(ll)) {
+    this.rerender();
+  }
+}
+
+PlannerPlugin.prototype.handleDrawTools = function(payload) {
+  // console.log("PLAN handleDrawTools", payload)
+  if (!payload) {
+    return;
+  }
+
+  if (payload.event === "clear") {
+    this.items = []
+    this.rerender();
+  } 
+  else if (payload.event === "layerCreated") {
+    this.addDrawToolsLayer(payload.layer);
+    this.rerender();
+  }
+  else if (payload.event === "layersDeleted" || payload.event === "import") {
+    // we dont get notified what was deleted, clobber everything for now
+    this.loadPlanFromDrawtools(JSON.parse(localStorage['plugin-draw-tools-layer']))
+    this.rerender();
+  }
+}
+
+
+
+PlannerPlugin.prototype.loadPlanFromDrawtools = function(drawToolsItems) {
+  this.items = [];
+  drawToolsItems.forEach(function(drawItem) {
+    this.addDrawToolsLayer(drawItem);
+  }.bind(this));
+}
+
+PlannerPlugin.prototype.addDrawToolsLayer = function(layer) {
+  var latlngs = layer.latLngs || layer._latlngs
+
+  if (latlngs.length == 2) {
+    this.addLink(latlngs[0], latlngs[1]);
+  } else if (latlngs.length == 3) {
+    this.addLink(latlngs[0], latlngs[1]);
+    this.addLink(latlngs[0], latlngs[2]);
+    this.addLink(latlngs[1], latlngs[2]);
+  }
+}
+
+PlannerPlugin.prototype.addLink = function(src, dest)  {
+  var newItem = new PlanItem(src, dest);
+  // console.log("PLAN addLink", newItem.uniqid())
+  if (this.items.findIndex(function(i) { return i.uniqid() == newItem.uniqid(); }) == -1) {
+    this.items.push(newItem)
+  }
+}
+
+PlannerPlugin.prototype.portalFromLatlng = function(latlng) {
     var ll = llstring(latlng)
     var portal = this.portal_by_ll[ll]
     return portal;
 }
-window.plugin.planner.portalTitle = function(portal) {
+
+PlannerPlugin.prototype.portalTitle = function(portal) {
     return portal ? 
       (portal.options.data.title || llstring(portal._latlng)) : 
       portal 
       ;
 }
 
-window.plugin.planner.renderPlanViewer = function(plan) {
+PlannerPlugin.prototype.renderPlanViewer = function(plan) {
   this.container = document.createElement('table');
   this.rerender();
   return this.container;
 };
 
-window.plugin.planner.rerender = function() {
+PlannerPlugin.prototype.rerender = function() {
   if (!this.container) return;
   this.container.innerHTML = "";
 
@@ -190,24 +227,24 @@ window.plugin.planner.rerender = function() {
 
 }
 
-window.plugin.planner.sortItemsBySource = function() {
+PlannerPlugin.prototype.sortItemsBySource = function() {
   this.items.sort(function(a, b) {
     return llstring(a.src).localeCompare(llstring(b.src))
   })
 }
 
-window.plugin.planner.moveStepUp = function(stepIdx) {
+PlannerPlugin.prototype.moveStepUp = function(stepIdx) {
   if (stepIdx > 0) {
     this.moveByIndex(this.items, stepIdx, -1);
   }
 }
-window.plugin.planner.moveStepDown = function(stepIdx) {
+PlannerPlugin.prototype.moveStepDown = function(stepIdx) {
   if (stepIdx < this.items.length-1) {
     this.moveByIndex(this.items, stepIdx, 1);
   }
 }
 
-window.plugin.planner.moveByIndex = function(array, index, delta) {
+PlannerPlugin.prototype.moveByIndex = function(array, index, delta) {
   //ref: https://gist.github.com/albertein/4496103
   var newIndex = index + delta;
   if (newIndex < 0 || newIndex == array.length) return; //Already at the top or bottom.
@@ -215,7 +252,7 @@ window.plugin.planner.moveByIndex = function(array, index, delta) {
   array.splice(indexes[0], 2, array[indexes[1]], array[indexes[0]]); //Replace from lowest index, two elements, reverting the order
 }
 
-window.plugin.planner.renderStep = function(step, stepIdx, previous) {
+PlannerPlugin.prototype.renderStep = function(step, stepIdx, previous) {
   var container = document.createElement('tr');
   container.className = 'plugin-planner-step';
 
@@ -263,7 +300,7 @@ window.plugin.planner.renderStep = function(step, stepIdx, previous) {
   return container;
 };
 
-window.plugin.planner.showPlannerDialog = function() {
+PlannerPlugin.prototype.showPlannerDialog = function() {
 
   dialog({
     html: this.renderPlanViewer(this.items),
@@ -277,45 +314,19 @@ window.plugin.planner.showPlannerDialog = function() {
 
 };
 
-window.plugin.planner.collapseFix = function() {
+PlannerPlugin.prototype.collapseFix = function() {
   if (this && this.parentNode) {
     this.parentNode.style.height = 'auto';
   }
 }
 
-window.plugin.planner.handlePaneChanged = function(pane) {
-  if(pane == 'plugin-planner') {
-    document.body.appendChild(this.mobilePane);
-  } else if(this.mobilePane.parentNode) {
-    this.mobilePane.parentNode.removeChild(this.mobilePane);
-  }
-};
 
-window.plugin.planner.handleDrawTools = function(payload) {
-  console.log("PLAN handleDrawTools", payload)
-  if (!payload) {
-    return;
-  }
 
-  if (payload.event === "clear") {
-    this.items = []
-    this.rerender();
-  } 
-  else if (payload.event === "layerCreated") {
-    this.addDrawToolsLayer(payload.layer);
-    this.rerender();
-  }
-  else if (payload.event === "layersDeleted" || payload.event === "import") {
-    // we dont get notified what was deleted, clobber everything for now
-    this.loadPlanFromDrawtools(JSON.parse(localStorage['plugin-draw-tools-layer']))
-    this.rerender();
-  }
-}
+
 
 
 // PLUGIN END //////////////////////////////////////////////////////////
-
-var setup =  window.plugin.planner.setup.bind(window.plugin.planner);
+var setup = PlannerPlugin.boot;
 
 setup.info = plugin_info; //add the script info data to the function as a property
 if(!window.bootPlugins) window.bootPlugins = [];
@@ -329,5 +340,3 @@ var info = {};
 if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
 script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
 (document.body || document.head || document.documentElement).appendChild(script);
-
-
